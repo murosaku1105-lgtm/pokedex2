50fe1fb5f31ecd1e131f49e5fd05f8eea2301d9b
MUROZUKA <murosaku1105@gmail.com>
2025-12-26 12:00:39 +0900

    検索画面実装段階2

--- MyPokedex.js
+++ MyPokedex.js
@@ -1,440 +1,173 @@
-const PokeProject = (() => {
+const PokeSearch = (() => {
   'use strict';
   let
-  //ページのターゲットNo
-  pokeTargetNo,
-
-  //最小/最大のNo取得
-  pokeMinNo,
-  pokeMaxNo,
-
-  //データ関連
-  pokeNo,
-  pokeName,
-  pokeType1,
-  pokeType2,
-  pokeUnionType,
-  pokeRegion,
-  pokeGen,
-  //pokePathNormal,
-  //pokePathShiny,
-  pokeDisplayImage,
-
-  //HTML関連(位置判定)
-  areaPageTitle,
-  areaPokeImage,
-  areaPokeTitle,
-  areaPokeType,
-  areaPokeInfo,
-  areaControlButtons,
-  areaSearchButton,
-  areaDisplayPokeList,
-
-  //HTML関連(挿入)
-  htmlPageTitle,
-  htmlPokeImage,
-  htmlPokeTitle,
-  htmlPokeType,
-  htmlPokeInfo,
-  htmlControlButtons,
-  htmlSearchButton,
-
-  //js上で必要となり得る要素
+  area_greeting,
+  html_greeting,
+  area_contents,
+  html_contents,
   func,
   flag,
-
-  //画像の切り替え
-  whichImage,
-  imageNormal,
-  imageShiny,
-
   active;
 
-  imageNormal = 'NORMAL'
-  imageShiny = 'SHINY'
+  let selectedTypes = [];
 
   const conf ={
-    fieldPageTitle: `field-page-title`,
-    fieldPokeImage: `field-selected-poke-image`,
-    fieldPokeTitle: `field-selected-poke-title`,
-    fieldPokeType: `field-selected-poke-type`,
-    fieldPokeInfo: `field-selected-poke-info`,
-    fieldControlButtons: `field-control-buttons`,
-    fieldDisplayPokeList: `field-display-poke-list`,
-    fieldSearchButton: `field-search-button`,
+    search_field: `search-field`,
   };
   func = {
     init: function(){
       flag = true;
-      active = true;
-      pokeTargetNo = 1;
-      whichImage = imageNormal;
       return this;
     },
-    makeFieldPageTitle(){
+    initTypeTable: function(){
       if(flag){
-        areaPageTitle = document.querySelector(`[${conf.fieldPageTitle}]`);
-        htmlPageTitle =
-        `<h1>～ My PokeDex ～</h1>`;
-
-        areaPageTitle.insertAdjacentHTML('beforeend', htmlPageTitle);
-
+        fetch('/api/poke/type')
+          .then(res => res.json())
+          .then(types => func.renderTypeTable(types));
       }
       return this;
     },
-    makeFieldControlButtons(){
+    renderTypeTable: function(types){
       if(flag){
-        areaControlButtons = document.querySelector(`[${conf.fieldControlButtons}]`);
-        htmlControlButtons =
-        `
-        <button id="BtnPrevPoke">⇐</button>
-        <button id="BtnSwitchImage">通常/色違い</button>
-        <button id="BtnNextPoke">⇒</button>
-        `;
-        areaControlButtons.insertAdjacentHTML('beforeend', htmlControlButtons);
+        const areaTypes = document.getElementById('TblSearchType');
+        areaTypes.innerHTML = "";
 
-        //current-1の個体にリンク
-        let BtnPrevPoke = document.getElementById('BtnPrevPoke');
-        if(BtnPrevPoke)
-          BtnPrevPoke.addEventListener('click', func.prevPoke);
-
-        //current+1の個体にリンク
-        let BtnNextPoke = document.getElementById('BtnNextPoke');
-        if(BtnNextPoke)
-          BtnNextPoke.addEventListener('click', func.nextPoke);
-
-        //画像を通常⇔色違いに反転
-        let BtnSwitchImage = document.getElementById('BtnSwitchImage');
-        if(BtnSwitchImage)
-          BtnSwitchImage.addEventListener('click', func.switchImage);
+        let row;
+        types.forEach((t, i) => {
+          if(i % 6 === 0){
+            row = document.createElement("div");
+            row.classList.add("type-row");
+            areaTypes.appendChild(row);
+          }
+          const img = document.createElement("img");
+          img.src= t.pathtype;
+          img.dataset.typeid = t.typeid;
+          img.classList.add("type-icon");
+          img.addEventListener("click", () => func.toggleTypeSelection(img));
+          row.appendChild(img);
+        });
       }
       return this;
     },
-    makeFieldSearchButton(){
+    initRegionList: function(){
       if(flag){
-        areaSearchButton = document.querySelector(`[${conf.fieldSearchButton}]`);
-        htmlSearchButton =
-        `<button id="BtnSearch">検索</button>`;
-        //MySearchdex.htmlへリンクさせたい
-        areaSearchButton.insertAdjacentHTML('beforeend', htmlSearchButton);
-
-        let BtnSearch = document.getElementById(`BtnSearch`);
-        if(BtnSearch)
-          BtnSearch.addEventListener('click', func.linkSearch);
+        fetch("/api/poke/region")
+          .then(res => res.json())
+          .then(regions => func.renderRegionList(regions));
       }
       return this;
     },
-    linkSearch(){
+    renderRegionList(regions){
       if(flag){
-        window.location.href = "MySearchdex.html";
+        const ddlRegion = document.getElementById("DdlSearchRegion");
+
+        regions.forEach(r => {
+          const opt = document.createElement("option");
+          opt.value = r.regionid;
+          opt.textContent = `${r.regionid}: ${r.region}`;
+          ddlRegion.appendChild(opt);
+        });
       }
       return this;
     },
-    getMinNo: async function(){
+    initGenList: function(){
       if(flag){
-        try{
-          const res = await fetch("http://localhost:3001/api/poke/minNo");
-          const data = await res.json();
-          pokeMinNo = Number(data.min);
-        }
-        catch (error) {
-          console.error('Error fetching data:', error);
-        }
-
+        fetch("/api/poke/gen")
+          .then(res => res.json())
+          .then(gens => func.renderGenList(gens));
       }
       return this;
     },
-    getMaxNo: async function(){
+    renderGenList: function(gens){
       if(flag){
-        try{
-          const res = await fetch("http://localhost:3001/api/poke/maxNo");
-          const data = await res.json();
-          pokeMaxNo = Number(data.max);
-        }
-        catch (error) {
-          console.error('Error fetching data:', error);
-        }
+        const ddlGen = document.getElementById("DdlSearchGen");
+
+        gens.forEach(g => {
+          const opt = document.createElement("option");
+          opt.value = g.genid;
+          opt.textContent = `${g.genid}: ${g.gen}`;
+          ddlGen.appendChild(opt);
+        });
       }
       return this;
     },
-    //getPokeData: async function
-    getPokeData: async function(getNo){
+    searchPoke: function(){
       if(flag){
-        if(getNo !== undefined)
-          pokeTargetNo = getNo
-        
-        try{
-          const res = await fetch(`http://localhost:3001/api/poke?pokeid=${pokeTargetNo}`);
-          const data = await res.json();
-          pokeNo = data.pokeid;
-            pokeName = data.name;
-            pokeType1 = data.type1;
-            pokeType2 = data.type2;
-            pokeRegion = data.region;
-            pokeGen = data.gen;
+        const name = document.getElementById("TxtSearchName").value;
+        const types = selectedTypes;
+        const region = document.getElementById("DdlSearchRegion").value;
+        const gen = document.getElementById("DdlSearchGen").value;
 
-            if(whichImage === imageNormal)
-            {
-              pokeDisplayImage = data.pathnormal;
-            }
-            else if(whichImage === imageShiny)
-            {
-              pokeDisplayImage = data.pathshiny;
-            }
-            else
-            {
-              pokeDisplayImage = data.pathnormal;
-            }
-
-            //タイプの調整
-            if(pokeType2 === null)
-            {
-              pokeUnionType = pokeType1;
-            }
-            else
-            {
-              pokeUnionType = pokeType1 + "/" + pokeType2;
-            }
-
-            //画像の判断
-            func
-              .initFieldPokeData()
-              .makeFieldPokeImage()
-              .makeFieldPokeTitle()
-              .makeFieldPokeType()
-              .makeFieldPokeInfo()
-        }
-        catch (error){
-          console.error('Error fetching data:', error);
-        }
+        fetch("/api/poke/search", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({name, types, region, gen})
+        })
+        .then(res => res.json())
+        .then(data => func.renderSearchResult(data));
       }
       return this;
     },
-    initFieldPokeData(){
+    toggleTypeSelection: function(img){
       if(flag){
-        areaPokeImage = document.querySelector(`[${conf.fieldPokeImage}]`);
-        areaPokeTitle = document.querySelector(`[${conf.fieldPokeTitle}]`);
-        areaPokeType = document.querySelector(`[${conf.fieldPokeType}]`);
-        areaPokeInfo = document.querySelector(`[${conf.fieldPokeInfo}]`);
+        const typeId = Number(img.dataset.typeid);
 
-        areaPokeImage.innerHTML = '';
-        areaPokeTitle.innerHTML = '';
-        areaPokeType.innerHTML = '';
-        areaPokeInfo.innerHTML = '';
-
-      }
-      return this;
-    },
-    makeFieldPokeImage(){
-      if(flag){
-        areaPokeImage = document.querySelector(`[${conf.fieldPokeImage}]`);
-        htmlPokeImage =
-        `<img id="pokeImage" src="${pokeDisplayImage}">`;
-
-        areaPokeImage.insertAdjacentHTML('beforeend', htmlPokeImage);
-      }
-      return this;
-    },
-    makeFieldPokeTitle(){
-      if(flag){
-        areaPokeTitle = document.querySelector(`[${conf.fieldPokeTitle}]`);
-        htmlPokeTitle =
-        `<p id="pokeNo" tag="${pokeNo}">No: ${pokeNo}</p><p id="pokeName">なまえ: ${pokeName}</p>`;
-
-        areaPokeTitle.insertAdjacentHTML('beforeend', htmlPokeTitle);
-      }
-      return this;
-    },
-    makeFieldPokeType(){
-      if(flag){
-        areaPokeType = document.querySelector(`[${conf.fieldPokeType}]`);
-        htmlPokeType =
-        `<p id="pokeType">タイプ: ${pokeUnionType}</p>`;
-
-        areaPokeType.insertAdjacentHTML('beforeend', htmlPokeType);
-      }
-      return this;
-    },
-    makeFieldPokeInfo(){
-      if(flag){
-        areaPokeInfo = document.querySelector(`[${conf.fieldPokeInfo}]`);
-        htmlPokeInfo =
-        `<p id="pokeRegion">地方: ${pokeRegion}</p><p id="pokeGen">地方: ${pokeGen}</p>`;
-
-        areaPokeInfo.insertAdjacentHTML('beforeend', htmlPokeInfo);
-      }
-      return this;
-    },
-    //ボタンの挙動メソッド
-    prevPoke(){
-      if(flag){
-
-        //現在のNoを取得
-        let elementNo = document.getElementById('pokeNo');
-        let currentNo = Number(elementNo.getAttribute('tag'));
-        let prevTargetNo = currentNo - 1;
-        localStorage.setItem('pokeMaxNo', pokeMaxNo);
-
-        if(prevTargetNo >= pokeMinNo)
+        if(selectedTypes.includes(typeId))
         {
-          //現No-1した個体を参照
-          func.getPokeData(prevTargetNo);
+          selectedTypes = selectedTypes.filter(x => x !== typeId);
+          img.classList.remove("selected");
         }
         else
         {
-          //最大Noの個体を参照
-          func.getPokeData(pokeMaxNo);
+          selectedTypes.push(typeId);
+          img.classList.add("selected");
         }
       }
       return this;
     },
-    nextPoke(){
+    renderSearchResult: function(list){
       if(flag){
-        let elementNo = document.getElementById('pokeNo');
-        let currentNo = Number(elementNo.getAttribute('tag'));
-        let nextTargetNo = currentNo + 1;
-        localStorage.setItem('pokeMinNo', pokeMinNo);
+        const body = document.getElementById("SearchResultBody");
+        body.innerHTML = "";
 
-        if(nextTargetNo <= pokeMaxNo)
+        if(list.length ===0)
         {
-          //現No+1した個体を参照
-          func.getPokeData(nextTargetNo);
-        }
-        else
-        {
-          //最小Noの個体を参照
-          func.getPokeData(pokeMinNo);
+          document.getElementById("LblNoResult").style.display = "block";
+          return;
         }
 
-      }
-      return this;
-    },
-    switchImage(){
-      if(flag){
-
-        if(whichImage === imageNormal)
-        {
-          whichImage = imageShiny;
-        }
-        else if(whichImage === imageShiny)
-        {
-          whichImage = imageNormal;
-        }
-        else
-        {
-          whichImage = imageNormal;
-        }
-        
-        let elementNo = document.getElementById('pokeNo');
-        let currentNo = Number(elementNo.getAttribute('tag'));
-        //currentNo = Number(document.getElementById('pokeNo').getAttribute('tag'));でも同様の結果取得可能
-        func.getPokeData(currentNo);
-        func.makeFieldDisplayPokeList();
-
-      }
-      return this;
-    },
-    makeFieldDisplayPokeList: async function(){
-      if(flag){
-        try{
-          const response = await fetch('/api/pokelist');
-          const data = await response.json();
-
-          areaDisplayPokeList = document.querySelector(`[${conf.fieldDisplayPokeList}]`);
-          areaDisplayPokeList.innerHTML = '';
-
-          data.forEach(pokemon => {
-            const img = document.createElement('img');
-            if(whichImage === imageNormal)
-            {
-              img.src = pokemon.pathnormal;
-            }
-            else if(whichImage === imageShiny)
-            {
-              img.src = pokemon.pathshiny;
-            }
-            else
-            {
-              img.src = pokemon.pathnormal;
-            }
-            img.alt = `Pokemon ${pokemon.pokeid}`;
-            img.dataset.pokeid = pokemon.pokeid;
-            img.classList.add('poke-image');
-
-            img.addEventListener('click', () => {
-              func.getPokeData(pokemon.pokeid);
-            });
-
-            areaDisplayPokeList.appendChild(img);
-          })
-
-        }
-        catch (error) {
-          console.error('ポケモンリストの獲得に失敗しました。', error);
-        }
+        document.getElementById("LblNoResult").style.display = "none";
+        list.forEach(p => {
+          const tr = document.createElement("tr");
+          tr.innerHTML =
+          `
+          <td><img src="${p.pathnormal}" class="middle-each-image"></td>
+          <td>${p.pokeid}</td>
+          <td>${p.name}</td>
+          <td>${p.type1}${p.type2 ? "・" + p.type2 : ""}</td>
+          <td>${p.region}</td>
+          <td>${p.gen}</td>
+          <td><a href="MyPokedex.html?No=${p.pokeid}">リンク</a></td>
+          `;
+          body.appendChild(tr);
+        });
       }
       return this;
     }
   };
 
-  active = async () => {
+  active = () => {
     func
       .init()
-      .makeFieldPageTitle()
-      .makeFieldControlButtons()
-      .makeFieldSearchButton();
-
-      await func.getMinNo();
-      await func.getMaxNo();
-      console.log(pokeMinNo, pokeMaxNo);
-      await func.getPokeData();
-      await func.makeFieldDisplayPokeList();
+      .initTypeTable()
+      .initRegionList()
+      .initGenList()
+      .searchPoke();
     return;
   }
   return (active);
 })();
 
 window.addEventListener('load', function(){
-  PokeProject();
+  PokeSearch();
 });
-//メソッド全体図
-
-//init
-//makeFieldTitle
-//makeFieldControlButtons
-//	[prevPoke]
-//	[nextPoke]
-//	[switchImage]
-
-//getMinNo(async)
-//getMaxNo(async)
-//getPokeData(async)
-//	[initFieldPokeData]
-//	[makeFieldPokeImage]
-//	[makeFieldPokeTitle]
-//	[makeFieldPokeType]
-//	[makeFieldPokeInfo]
-
-//initFieldPokeData
-//makeFieldPokeImage
-//makeFieldPokeTitle
-//makeFieldPokeType
-//makeFieldPokeInfo
-
-//prevPoke
-//nextPoke
-//switchImage
-//	[getPokeData]
-//	[makeFieldDisplayPokeList]
-
-//makeFieldDisplayPokeList(async)
-
-
-//active
-//	[init]
-//	[makeFieldPageTitle]
-//	[makeFieldControlButtons]
-//	[getMinNo]
-//	[getMaxNo]
-//	[getPokeData]
-//	[makeFieldDisplayPokeList]
